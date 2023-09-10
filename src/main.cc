6540f7ec1bccdc99584ed2978549025e883605f5
lafette21 <viragh.akos21@gmail.com>
2023-09-10 15:59:19 +0200

    feat: Add ImGui wrappers

--- src/main.cc
+++ src/main.cc
@@ -1,10 +1,12 @@
+#include "yui/yui.h"
+
 #include <fmt/format.h>
 
 #include <cstdlib>
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[])
 {
-    fmt::print("Example {}", "project");
+    fmt::print("Example {}\n", "project");
 
     return EXIT_SUCCESS;
 }
