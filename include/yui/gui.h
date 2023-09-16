8f5e36c9327ab2f64382f75cffac348a5c9c37b9
GK72 <gk.project72@gmail.com>
2023-09-16 13:24:12 +0200

    feat: reusability

--- include/yui/gui.h
+++ include/yui/gui.h
@@ -14,12 +14,51 @@
 
 namespace yui {
 
+struct gui_config {
+    vec2 window_size;
+    std::string fontpath;
+    float fontsize;
+};
+
+class image {
+public:
+    image(yui::vec2 dimensions)
+        : m_dimensions(dimensions)
+        , m_data(
+            static_cast<std::size_t>(area(dimensions)),
+            0xFFFFFFFF
+        )
+    {}
+
+    image(const image&) = delete;
+    image(image&&) = delete;
+    image& operator=(const image&) = delete;
+    image& operator=(image&&) = delete;
+
+    [[nodiscard]] const auto* data() const noexcept { return m_data.data(); }
+    [[nodiscard]] auto* data()             noexcept { return m_data.data(); }
+    [[nodiscard]] auto width()       const noexcept { return m_dimensions.x(); }
+    [[nodiscard]] auto height()      const noexcept { return m_dimensions.y(); }
+    [[nodiscard]] auto dimensions()  const noexcept { return m_dimensions; }
+    [[nodiscard]] auto& texture_id()       noexcept { return m_texture_id; }
+
+    [[nodiscard]] auto& at(int x, int y) noexcept {
+        using SizeT = decltype(m_data)::size_type;
+        return m_data[static_cast<SizeT>(x * static_cast<int>(height()) + y)];
+    }
+
+private:
+    yui::vec2 m_dimensions;
+    std::vector<std::uint32_t> m_data;
+    unsigned int m_texture_id;
+};
+
 /**
  * @brief   Wrapper class around GLFW and ImGUI
  */
 class gui {
 public:
-    gui(const std::string& title, vec2 size);
+    gui(const std::string& title, const gui_config& cfg);
     ~gui();
 
     gui(const gui&)             = delete;
@@ -48,6 +87,25 @@ public:
      */
     class window window(const std::string& title, const window_config& config, bool* is_open = nullptr) {
         return yui::window(title, config, is_open);
+    }
+
+    /**
+     * @brief   Display an image
+     */
+    GLuint load(class image& img) {
+        // Create a OpenGL texture identifier
+        glGenTextures(1, &img.texture_id());
+        glBindTexture(GL_TEXTURE_2D, img.texture_id());
+
+        // Setup filtering parameters for display
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, static_cast<int>(img.width()), static_cast<int>(img.height()), 0, GL_RGBA, GL_UNSIGNED_BYTE, img.data());
+
+        return img.texture_id();
     }
 
     [[nodiscard]] inline auto dimensions() const noexcept { return m_dimensions; }
--- include/yui/types.h
+++ include/yui/types.h
@@ -3,9 +3,15 @@
 
 #include <imgui.h>
 
+#include <algorithm>
+#include <cstdint>
 #include <compare>
+#include <type_traits>
 
 namespace yui {
+
+template <class T>
+concept arithmetic = std::is_arithmetic_v<T>;
 
 /**
  * @brief   Internal vec2 type
@@ -19,9 +25,9 @@ struct vec2 {
         data{ obj.x, obj.y }
     {}
 
-    auto operator<=>(const vec2&) const = default;
+    constexpr auto operator<=>(const vec2&) const = default;
 
-    inline vec2& operator+=(const vec2& obj) noexcept {
+    constexpr inline vec2& operator+=(const vec2& obj) noexcept {
         data[0] += obj.data[0];
         data[1] += obj.data[1];
         return *this;
@@ -30,7 +36,7 @@ struct vec2 {
     /**
      * @brief   Division by a scalar
      */
-    inline vec2& operator/=(float scalar) noexcept {
+    constexpr inline vec2& operator/=(float scalar) noexcept {
         data[0] /= scalar;
         data[1] /= scalar;
         return *this;
@@ -40,55 +46,55 @@ struct vec2 {
         return { data[0], data[1] };
     }
 
-    float& x() { return data[0]; }
-    float& y() { return data[1]; }
+    constexpr float& x() { return data[0]; }
+    constexpr float& y() { return data[1]; }
 
-    float x() const { return data[0]; }
-    float y() const { return data[1]; }
+    constexpr float x() const { return data[0]; }
+    constexpr float y() const { return data[1]; }
 
     float data[2];
 };
 
-inline vec2 operator+(const vec2& lhs, const vec2& rhs) noexcept {
+constexpr inline vec2 operator+(const vec2& lhs, const vec2& rhs) noexcept {
     return {
         lhs.data[0] + rhs.data[0],
         lhs.data[1] + rhs.data[1]
     };
 }
 
-inline vec2 operator+(const ImVec2& lhs, const vec2& rhs) noexcept {
+constexpr inline vec2 operator+(const ImVec2& lhs, const vec2& rhs) noexcept {
     return {
         lhs.x + rhs.data[0],
         lhs.y + rhs.data[1]
     };
 }
 
-inline vec2 operator+(const vec2& lhs, const ImVec2& rhs) noexcept {
+constexpr inline vec2 operator+(const vec2& lhs, const ImVec2& rhs) noexcept {
     return rhs + lhs;
 }
 
-inline vec2 operator-(const vec2& lhs, const vec2& rhs) noexcept {
+constexpr inline vec2 operator-(const vec2& lhs, const vec2& rhs) noexcept {
     return {
         lhs.data[0] - rhs.data[0],
         lhs.data[1] - rhs.data[1]
     };
 }
 
-inline vec2 operator-(const ImVec2& lhs, const vec2& rhs) noexcept {
+constexpr inline vec2 operator-(const ImVec2& lhs, const vec2& rhs) noexcept {
     return {
         lhs.x - rhs.data[0],
         lhs.y - rhs.data[1]
     };
 }
 
-inline vec2 operator-(const vec2& lhs, const ImVec2& rhs) noexcept {
+constexpr inline vec2 operator-(const vec2& lhs, const ImVec2& rhs) noexcept {
     return rhs - lhs;
 }
 
 /**
  * @brief:   Element-wise multiplication
  */
-inline vec2 operator*(const vec2& lhs, const vec2& rhs) noexcept {
+constexpr inline vec2 operator*(const vec2& lhs, const vec2& rhs) noexcept {
     return {
         lhs.data[0] * rhs.data[0],
         lhs.data[1] * rhs.data[1]
@@ -98,7 +104,7 @@ inline vec2 operator*(const vec2& lhs, const vec2& rhs) noexcept {
 /**
  * @brief   Element-wise division
  */
-inline vec2 operator/(const vec2& lhs, const vec2& rhs) noexcept {
+constexpr inline vec2 operator/(const vec2& lhs, const vec2& rhs) noexcept {
     return {
         lhs.data[0] / rhs.data[0],
         lhs.data[1] / rhs.data[1]
@@ -108,7 +114,7 @@ inline vec2 operator/(const vec2& lhs, const vec2& rhs) noexcept {
 /**
  * @brief   Division by a scalar
  */
-inline vec2 operator/(const vec2& lhs, float rhs) noexcept {
+constexpr inline vec2 operator/(const vec2& lhs, float rhs) noexcept {
     return {
         lhs.data[0] / rhs,
         lhs.data[1] / rhs
@@ -119,15 +125,15 @@ inline vec2 operator/(const vec2& lhs, float rhs) noexcept {
  * @brief   Internal vec4 type
  */
 struct vec4 {
-    float x() const { return data[0]; }
-    float y() const { return data[1]; }
-    float z() const { return data[2]; }
-    float w() const { return data[3]; }
+    constexpr float x() const { return data[0]; }
+    constexpr float y() const { return data[1]; }
+    constexpr float z() const { return data[2]; }
+    constexpr float w() const { return data[3]; }
 
-    float r() const { return data[0]; }
-    float g() const { return data[1]; }
-    float b() const { return data[2]; }
-    float a() const { return data[3]; }
+    constexpr float r() const { return data[0]; }
+    constexpr float g() const { return data[1]; }
+    constexpr float b() const { return data[2]; }
+    constexpr float a() const { return data[3]; }
 
     float data[4];
 };
@@ -135,7 +141,57 @@ struct vec4 {
 namespace color {
     constexpr auto black = vec4{   0.0f,   0.0f,   0.0f, 255.0f };
     constexpr auto white = vec4{ 255.0f, 255.0f, 255.0f, 255.0f };
+    constexpr auto blue  = vec4{   0.0f,   0.0f, 255.0f, 255.0f };
 }
+
+/**
+ * @brief   Clamp between 0 and 255 for packing four 8-bit values
+ */
+[[nodiscard]] constexpr uint32_t cast8(arithmetic auto x) noexcept {
+    return std::clamp(static_cast<uint32_t>(x), 0u, 255u);
+}
+
+/**
+ * @brief   Pack 4 8-bit values (Little endian)
+ */
+template <arithmetic T>
+[[nodiscard]] constexpr std::uint32_t pack32LE(T x, T y, T z, T w) noexcept {
+    return cast8(w) << 24
+         | cast8(z) << 16
+         | cast8(y) << 8
+         | cast8(x);
+}
+
+/**
+ * @brief   Pack 4 8-bit values (Big endian)
+ */
+template <arithmetic T>
+[[nodiscard]] constexpr std::uint32_t pack32BE(T x, T y, T z, T w) noexcept {
+    return pack32LE(w, z, y, x);
+}
+
+constexpr inline auto area(vec2 dimensions) noexcept {
+    return dimensions.x() * dimensions.y();
+}
+
+[[nodiscard]] constexpr std::uint32_t pack32LE(vec4 value) noexcept {
+    return pack32LE(
+        value.x(),
+        value.y(),
+        value.z(),
+        value.w()
+    );
+}
+
+[[nodiscard]] constexpr std::uint32_t pack32BE(vec4 value) noexcept {
+    return pack32BE(
+        value.x(),
+        value.y(),
+        value.z(),
+        value.w()
+    );
+}
+
 
 } // namespace yui
 
