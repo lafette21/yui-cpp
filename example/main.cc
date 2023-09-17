19772ac657c9b59e455af669591c23cfc6240f9c
laFette21 <viragh.akos21@gmail.com>
2023-09-17 16:56:54 +0200

    feat: Add slider and drag_slider implementations

--- example/main.cc
+++ example/main.cc
@@ -11,8 +11,15 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[])
     try {
         yui::gui gui{"Yui", yui::gui_config{ yui::vec2{ 1280, 720 }, {}, {} }};
 
-        gui.run([&gui] {
+        int ivalue = 0;
+        float fvalue = 0;
+
+        gui.run([&] {
             gui.main_menu().menu("Foo");
+
+            gui.window("Test")
+                .slider("Slider1", &ivalue, -100, 100)
+                .drag_slider("DragSlider1", &fvalue, 1.0f, -100.0f, 100.0f);
         });
     } catch (const std::exception& ex) {
         fmt::print("Exception occured: {}\n", ex.what());
